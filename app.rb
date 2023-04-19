b299e0ba7b898fb0ec016352b04e1172bc894660
EC2 Default User <ec2-user@ip-10-1-4-189.ap-northeast-1.compute.internal>
2023-04-19 15:04:45 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require './models.rb'
+
+require 'net/http'
+require 'json'
+require 'openai'
+require 'dotenv'
+
+Dotenv.load
+
+enable :sessions
+
+get '/' do
+    
+    @answer = session[:result]
+  erb :index
+end
+
+post "/question" do
+    # APIアクセス
+    client = OpenAI::Client.new(access_token: "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")
+    
+    response = client.chat(
+    parameters: {
+        model: "gpt-3.5-turbo", # Required.
+        messages: [{ role: "user", content: params[:question] }], # Required.
+        temperature: 0.7,
+    })
+    res_dig = response.dig("choices", 0, "message", "content")
+    
+    session[:result] = res_dig
+    
+    redirect "/"
+end
+
+
